5b8031f9fc45e6c632c193940a6d4c9fd0d780f4
krisb <anandhukrishna10b@gmail.com>
2026-05-16 23:15:33 +0530

    wrote peek and consume and integrated

--- src/tokenization.h
+++ src/tokenization.h
@@ -18,60 +18,57 @@ struct Token
 
 class Tokenizer {
 public:
-    inline Tokenizer(std::string& src)
+    inline Tokenizer(std::string src)
         :m_src(std::move(src))
     {
     }
 
     inline std::vector<Token> tokenize()
     {
+        std::cout << "hi" << std::endl;
         std::vector<Token> tokens;
         std::string buf;
-        for (int i = 0; i < str.length(); i++)
+
+        while (peak().has_value())
         {
-            char c = str.at(i);
-            if (std::isalpha(c))
+            if (std::isalpha(peak().value()))
             {
-                buf.push_back(c);
-                i++;
-                while (std::isalnum(str.at(i)))
+                buf.push_back(consume());
+                while (peak().value() && std::isalnum(peak().value()))
                 {
-                    buf.push_back(str.at(i));
-                    i++;
+                    buf.push_back(consume());
                 }
-                i--;
 
                 if (buf == "exit")
                 {
                     tokens.push_back({.type = TokenType::exit});
                     buf.clear();
                     continue;
-                } else
+                }
+                else
                 {
                     std::cerr << "You messed up!" << std::endl;
                     exit(EXIT_FAILURE);
                 }
             }
-            else if (std::isdigit(c))
+            else if (std::isdigit(peak().value()))
             {
-                buf.push_back(c);
-                i++;
+                buf.push_back(consume());
 
-                while (std::isdigit(str.at(i)))
+                while (peak().has_value() && std::isdigit(peak().value()))
                 {
-                    buf.push_back(str.at(i));
-                    i++;
+                    buf.push_back(consume());
                 }
-                i--;
 
                 tokens.push_back({.type = TokenType::int_lit, .value = buf});
                 buf.clear();
+                continue;
             }
-            else if (c == ';')
+            else if (peak().value() == ';')
             {
                 tokens.push_back({.type = TokenType::semi});
             }
-            else if (std::isspace(c))
+            else if (std::isspace(peak().value()))
             {
                 continue;
             }
@@ -81,15 +78,15 @@ public:
                 exit(EXIT_FAILURE);
             }
         }
-
+        m_index = 0;
         return tokens;
     }
 
 private:
 
-    std::optional<char> peak const()
+    [[nodiscard]]std::optional<char> peak(int ahead = 1) const
     {
-        if (m_index + 1 >= m_src.length())
+        if (m_index + ahead >= m_src.length())
         {
             return {};
         } else
@@ -98,6 +95,11 @@ private:
         }
     }
 
+    char consume()
+    {
+        return m_src.at(m_index++);
+    }
+
     const std::string m_src;
-    int m_index;
+    int m_index = 0;
 };
